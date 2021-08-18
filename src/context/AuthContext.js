597d4e8606e9bf9c894b17e73dc55adc34e15975
Nick Boucher <nboucher866@gmail.com>
2021-08-18 14:15:28 -0600

    Handle Errored Requests

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,19 +1,23 @@
 import createDataContext from './createDataContext';
+import trackerApi from '../api/tracker';
 
 const authReducer = (state, action) => {
     switch (action.type) {
+        case 'add_error':
+            return { ...state, errorMessage: action.payload };
         default:
             return state;
     }
 };
 
 const signup = (dispatch) => {
-    return ({ email, password }) => {
-        // Make API request to sign up with that email and password
-
-        //if we signup, then modify my state and say that we are authenticated
-
-        //if signup fails I need to reflect an error message somewhere
+    return async ({ email, password }) => {
+        try {
+            const response = await trackerApi.post('/signup', { email, password });
+            console.log(response.data);
+        } catch (err) {
+            dispatch({ type: 'add_error', payload: 'Something went wrong with signup!' })
+        }
     };
 };
 
@@ -36,5 +40,5 @@ const signout = (dispatch) => {
 export const { Provider, Context } = createDataContext(
     authReducer, 
     { signin, signout, signup }, 
-    { isSignedIn: false }
+    { isSignedIn: false, errorMessage: '' }
 );
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Button, Input, Text  } from 'react-native-elements';
 import Spacer from '../components/Spacer';
+import { Context as AuthContext } from '../context/AuthContext';
 
 const SignupScreen = ({ navigation }) => {
+    const { state, signup } = useContext(AuthContext);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
@@ -15,8 +17,9 @@ const SignupScreen = ({ navigation }) => {
             <Input label="Email" value={email} onChangeText={setEmail} autoCapitalize="none" autoCorrect={false} />
             <Spacer />
             <Input secureTextEntry label="Password" value={password} onChangeText={setPassword} autoCapitalize="none" autoCorrect={false} />
+            {state.errorMessage ? <Text style={styles.errorMessage}>{state.errorMessage}</Text> : null}
             <Spacer>
-                <Button title="Sign Up" />
+                <Button title="Sign Up" onPress={() => signup({ email, password })} />
             </Spacer>
         </View>
     );
@@ -33,6 +36,12 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         marginBottom: 250
+    },
+    errorMessage: {
+        fontSize: 16,
+        color: 'red',
+        marginLeft: 15,
+        marginTop: 15
     }
 });
 
