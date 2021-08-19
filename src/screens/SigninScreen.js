bcb56fe47680f24766e6a574e739b7af8f5574f9
Nick Boucher <nboucher866@gmail.com>
2021-08-19 12:47:45 -0600

    Clear Error Messages on Signin Signup Screens

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,14 +1,16 @@
 import React, { useContext } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationEvents } from 'react-navigation';
 import AuthForm from '../components/AuthForm';
 import NavLink from '../components/NavLink';
 import { Context } from '../context/AuthContext';
 
 const SigninScreen = () => {
-    const { state, signin } = useContext(Context);
+    const { state, signin, clearErrorMessage } = useContext(Context);
 
     return (
         <View style={styles.container}>
+            <NavigationEvents onWillFocus={clearErrorMessage}/>
             <AuthForm headerText="Sign In to Your Account" errorMessage={state.errorMessage} onSubmit={signin} submitButtonText="Sign In" />
             <NavLink text="Dont have an account? Sign up instead!" routeName="Signup" />
         </View>
